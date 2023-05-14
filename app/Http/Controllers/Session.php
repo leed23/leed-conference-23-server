7d5ea2d40d01cac85d9ac17134eb5c0c095cac2d
Benjamin Davies <b.davies@mmu.ac.uk>
2023-05-14 13:49:19 +0100

    Added one to many relationship to sessions model and added themes to query builder in the sessions comtroller

--- app/Http/Controllers/Session.php
+++ app/Http/Controllers/Session.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
+
 use App\Models\Sessions;
 
 use Illuminate\Http\Request;
@@ -11,12 +13,17 @@ class Session extends Controller
 
    public function index() {
 
-   $filter = QueryBuilder::for(Sessions::class) 
-      ->allowedFilters('title', 'name')
-      ->get();
+      $filter = QueryBuilder::for(Sessions::class)
+         ->with('themes')
+         ->AllowedFilters([
+            AllowedFilter::exact('themes', 'themes.theme'),
+            AllowedFilter::exact('session_format'),
+         ])
+         ->get();
 
       return $filter;
    }
+
 
    public function show($id) {
       return Sessions::where('slug', $id)->first();
